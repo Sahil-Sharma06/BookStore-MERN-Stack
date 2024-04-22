36981a6b587673077b93c78bffb8b410d14f5ded
Sahil-Sharma06 <116885079+Sahil-Sharma06@users.noreply.github.com>
2024-04-22 12:18:33 +0530

    Refractor with node js and changes in index.js

--- backend/index.js
+++ backend/index.js
@@ -2,38 +2,16 @@ import express from "express";
 import {PORT, mongodbURL} from './config.js';
 import mongoose from "mongoose";
 import {Book} from './Models/Bookmodel.js'
+import bookRoute from "./Routes/bookRoute.js";
 const app = express();
 
-
+app.use(express.json());
 app.get('/', (req, res) => {
     console.log(req);
     return res.status(234).send('Welcome to BookStore in MERN Stack');
 });
 
-app.post('/books', async (req, res) => {
-try {
-    if(
-    !req.body.title||
-    !req.body.author||
-    !req.body.publishYear
-    ){
-        return res.status(400).send({message: 'Send all the required fiels : title, author, publishYear'});
-    }
-
-    const newBook = {
-        title: req.body.title,
-        author: req.body.author,
-        publishYear: req.body.publishYear
-    };
-
-    const book = await Book.create(newBook);
-    return res.status(201).send(book);
-} catch (error) {
-    console.log(error);
-    res.status(500).send({message: error.message});
-}
-})
-
+app.use('/books',bookRoute);
 
 mongoose.connect(mongodbURL)
 .then(()=>{
